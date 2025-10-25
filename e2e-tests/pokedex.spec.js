7788f81f50752dbe25e5703ae6bd1763cd9e4a1a
thomastoumasu <thomastoumasu@protonmail.com>
2025-10-25 11:17:06 +0200

    added test

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -6,4 +6,12 @@ describe("Pokedex", () => {
     await expect(page.getByText("ivysaur")).toBeVisible();
     await expect(page.getByText("Pokémon and Pokémon character names are trademarks of Nintendo.")).toBeVisible();
   });
+
+  test("pokemon page can be navigated to", async ({ page }) => {
+    await page.goto("");
+    const locator = page.getByText("ivysaur");
+    await expect(locator).toBeVisible();
+    await locator.click();
+    await expect(page.getByText("chlorophyll")).toBeVisible();
+  });
 });
