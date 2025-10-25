feddf26258b50824d53d9c517a7b83fc7bd46d3f
thomastoumasu <thomastoumasu@protonmail.com>
2025-10-25 16:19:26 +0200

    simulate a broken deployment

--- app.js
+++ app.js
@@ -7,10 +7,16 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static("dist"));
 
 app.get("/version", (req, res) => {
-  res.send("1.1"); // change this string to ensure a new version deployed
+  res.send("1.2"); // change this string to ensure a new version deployed
 });
 
-app.get("/health", (req, res) => {
+// app.get("/health", (req, res) => {
+//   res.send("ok"); // health check ensuring that the app deployed (for example in render) is in a functional state.
+// });
+
+app.get("/health", (req, res) => { // simulate a broken deployment
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw "error...  ";
   res.send("ok");
 });
 
